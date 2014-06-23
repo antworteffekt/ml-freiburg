fd9def71e54ea86d1a89ec7e7048e9930478f70b
mlotstein <mlotstein@yahoo.com>
2014-06-23 12:27:10 +0200

    Adding R Script for making confusion plots

--- task1/src/makeConfusionPlots.R
+++ task1/src/makeConfusionPlots.R
@@ -1,4 +1,4 @@
-setwd("~/GitHub/ml-freiburg/task1/src/Run2")
+setwd("C:/Users/Max/Documents/GitHub/ml-freiburg/task1/data/")
 results <- read.csv("errorTable18-Jun-2014.csv")
 
 # creates a own color palette from red to green
@@ -6,7 +6,7 @@ my_palette <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
 rownames <- c("trainbfg","trainbr","traingd","traingda","traingdm","traingdx","trainrp")
 
 for (tf in c("trainbfg","trainbr","traingd","traingda","traingdm","traingdx","trainrp")) {
-  s <- subset(results, TrainingFn=tf)
+  s <- results[which(results$TrainingFn== tf),] 
   s <- s[c(-1,-2,-3,-4,-5,-6,-7)]
   conf <- as.vector(colMeans(s))
   df <- data.frame(conf[1:3], conf[4:6], conf[7:9])
@@ -18,7 +18,7 @@ for (tf in c("trainbfg","trainbr","traingd","traingda","traingdm","traingdx","tr
 regularizers <- c(0.0, 0.1, 0.2, 0.5, 1.0)
 
 for (r in regularizers) {
-  s <- subset(results, Regularization=r)
+  s <- results[which(results$Regularization == r),] 
   s <- s[c(-1,-2,-3,-4,-5,-6,-7)]
   conf <- as.vector(colMeans(s))
   df <- data.frame(conf[1:3], conf[4:6], conf[7:9])
